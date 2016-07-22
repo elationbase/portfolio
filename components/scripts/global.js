206f203746511b8f585ce56091e1692026e68eab
elationbase <elationbase@gmail.com>
2016-07-22 19:38:30 -0400

    refactor portfolio hover effect

--- components/scripts/global.js
+++ components/scripts/global.js
@@ -1,124 +1,52 @@
 // remap jQuery to $
 (function($){
+	'use strict'
 
-	var getHoverDir = function( $element, x, y ) {
+	var $this, dir;
+	var info = '.info-container';
+	$(info).addClass('hover-top no-transition');
+
+	var getHoverDir = function( $ele, x, y ) {
 		// console.log( x + ", " + y );
-		var w = $element.width(),
-			h = $element.height(),
-			x = ( x - $element.offset().left - ( w/2 )) * ( w > h ? ( h/w ) : 1 ),
-			y = ( y - $element.offset().top  - ( h/2 )) * ( h > w ? ( w/h ) : 1 ),
+		var w = $ele.width(),
+			h = $ele.height(),
+			x = ( x - $ele.offset().left - ( w/2 )) * ( w > h ? ( h/w ) : 1 ),
+			y = ( y - $ele.offset().top  - ( h/2 )) * ( h > w ? ( w/h ) : 1 ),
 			direction = Math.round( ( ( ( Math.atan2(y, x) * (180 / Math.PI) ) + 180 ) / 90 ) + 3 ) % 4;
-		// console.log ( "direction> " + direction );
 		return direction;
 	}
 
-
-
-	var itemEnter;
-	var itemLeave;
-	var info = '.info-container';
-	var dir;
+	var hoverCase = function (dir, $ele) {
+		switch(dir) {
+			case 0:
+				$ele.find(info).addClass('hover-top');
+				break;
+			case 1:
+				$ele.find(info).addClass('hover-right');
+				break;
+			case 2:
+				$ele.find(info).addClass('hover-bottom');
+				break;
+			case 3:
+				$ele.find(info).addClass('hover-left');
+				break;
+			default:
+				break;
+		}
+	}
 
 	var prortfolioHover = function () {
 		$('.col a').on('mouseenter', function(event) {
-			itemEnter = $(this);
+			$this = $(this);
+			dir = getHoverDir( $this, event.pageX, event.pageY );
+			$this.find(info).removeClass('hover-top hover-right hover-bottom hover-left').addClass('no-transition');
+			hoverCase(dir, $this);
+			$this.find(info).removeClass('hover-top hover-right hover-bottom hover-left no-transition');
+		}).on( 'mouseleave', function( event ) {
+			$this = $(this);
 			dir = getHoverDir( $(this), event.pageX, event.pageY );
-			itemEnter.find(info).css({transition: 'none;'});
-			switch(dir) {
-				case 0:
-					itemEnter.find(info).css({
-						transition: 'none',
-						transform: 'translateY(-100%) translateX(0)',
-						MozTransform: 'translateY(-100%) translateX(0)',
-						WebkitTransform: 'translateY(-100%) translateX(0)',
-						msTransform: 'translateY(-100%) translateX(0)'
-					});
-					break;
-				case 1:
-					itemEnter.find(info).css({
-						transition: 'none',
-						transform: 'translateX(100%) translateY(0)',
-						MozTransform: 'translateX(100%) translateY(0)',
-						WebkitTransform: 'translateX(100%) translateY(0)',
-						msTransform: 'translateX(100%) translateY(0)'
-					});
-					break;
-				case 2:
-					itemEnter.find(info).css({
-						transition: 'none',
-						transform: 'translateX(-100%) translateY(0)',
-						MozTransform: 'translateX(-100%) translateY(0)',
-						WebkitTransform: 'translateX(-100%) translateY(0)',
-						msTransform: 'translateX(-100%) translateY(0)'
-					});
-					break;
-				case 3:
-					itemEnter.find(info).css({
-						transition: 'none',
-						transform: 'translateX(-100%) translateY(0)',
-						MozTransform: 'translateX(-100%) translateY(0)',
-						WebkitTransform: 'translateX(-100%) translateY(0)',
-						msTransform: 'translateX(-100%) translateY(0)'
-					});
-					break;
-				default:
-					break;
-			}
-
-			setTimeout(function() {
-				if ( itemEnter !== null ) {
-					itemEnter.find(info).css({
-						transition: 'all 0.6s ease',
-						transform: 'translateX(0) translateY(0)',
-						MozTransform: 'translateX(0) translateY(0)',
-						WebkitTransform: 'translateX(0) translateY(0)',
-						msTransform: 'translateX(0) translateY(0)'
-					});
-				}
-				itemEnter = null;
-			}, 30 );
-
-		});
-
-		$('.col a').on( 'mouseleave', function( event ) {
-			itemLeave = $( this );
-			dir = getHoverDir( $(this), event.pageX, event.pageY );
-			switch(dir) {
-				case 0:
-					itemLeave.find(info).css({
-						transform: 'translateY(-100%) translateX(0)',
-						MozTransform: 'translateY(-100%) translateX(0)',
-						WebkitTransform: 'translateY(-100%) translateX(0)',
-						msTransform: 'translateY(-100%) translateX(0)'
-					});
-					break;
-				case 1:
-					itemLeave.find(info).css({
-						transform: 'translateX(100%) translateY(0)',
-						MozTransform: 'translateX(100%) translateY(0)',
-						WebkitTransform: 'translateX(100%) translateY(0)',
-						msTransform: 'translateX(100%) translateY(0)'
-					});
-					break;
-				case 2:
-					itemLeave.find(info).css({
-						transform: 'translateY(100%) translateX(0)',
-						MozTransform: 'translateY(100%) translateX(0)',
-						WebkitTransform: 'translateY(100%) translateX(0)',
-						msTransform: 'translateY(100%) translateX(0)'
-					});
-					break;
-				case 3:
-					itemLeave.find(info).css({
-						transform: 'translateX(-100%) translateY(0)',
-						MozTransform: 'translateX(-100%) translateY(0)',
-						WebkitTransform: 'translateX(-100%) translateY(0)',
-						msTransform: 'translateX(-100%) translateY(0)'
-					});
-					break;
-				default:
-					break;
-			}
+			$this.find(info).removeClass('hover-top hover-right hover-bottom hover-left')
+			hoverCase(dir, $this);
 		});
 	}
 
@@ -152,14 +80,13 @@
 	};
 
 	var init = function() {
-		prortfolioHover ();
-		$(window).on('scroll', addStick);
+		prortfolioHover();
 	}
 
 
 	/* trigger when page is ready */
 	$(document).ready(init);
-
+	$(window).on('scroll', addStick);
 
 	/* optional triggers
 
